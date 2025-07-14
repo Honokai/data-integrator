9a784d88003ea6f2069e41d87853b0df0197a795
Honokai <Emerson_Hatred@hotmail.com>
2025-07-14 17:16:22 -0300

    início do projeto

---
+++ data-integrator-backend/src/main/java/dev/honokai/data_integrator_backend/DataIntegratorBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.honokai.data_integrator_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataIntegratorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataIntegratorBackendApplication.class, args);
+	}
+
+}
