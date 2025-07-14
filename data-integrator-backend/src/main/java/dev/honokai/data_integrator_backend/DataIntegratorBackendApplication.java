8b1e16eac9f1abb183d22f47872ca8d2ae677c42
Honokai <Emerson_Hatred@hotmail.com>
2025-07-14 17:30:21 -0300

    adicionando agendamento a aplicacao, e rabbitmq

--- data-integrator-backend/src/main/java/dev/honokai/data_integrator_backend/DataIntegratorBackendApplication.java
+++ data-integrator-backend/src/main/java/dev/honokai/data_integrator_backend/DataIntegratorBackendApplication.java
@@ -1,8 +1,12 @@
 package dev.honokai.data_integrator_backend;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableRabbit
+@EnableScheduling
 @SpringBootApplication
 public class DataIntegratorBackendApplication {
 
