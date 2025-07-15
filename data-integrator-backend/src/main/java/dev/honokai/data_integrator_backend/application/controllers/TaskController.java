3575e18d9097991605a7f37db4360b2f382c4394
Honokai <Emerson_Hatred@hotmail.com>
2025-07-15 15:52:02 -0300

    implementaçao de funcionalidades basicas para leitura, criacao e atualizacao, remocao sera tratada posteriormente

--- data-integrator-backend/src/main/java/dev/honokai/data_integrator_backend/application/controllers/TaskController.java
+++ data-integrator-backend/src/main/java/dev/honokai/data_integrator_backend/application/controllers/TaskController.java
@@ -1,12 +1,15 @@
 package dev.honokai.data_integrator_backend.application.controllers;
 
+import dev.honokai.data_integrator_backend.application.dtos.ScriptResponseDto;
 import dev.honokai.data_integrator_backend.application.dtos.TaskCreateDto;
 import dev.honokai.data_integrator_backend.application.dtos.TaskResponseDto;
+import dev.honokai.data_integrator_backend.application.dtos.TaskUpdateDto;
 import dev.honokai.data_integrator_backend.application.services.MachineService;
 import dev.honokai.data_integrator_backend.application.services.TaskService;
 import dev.honokai.data_integrator_backend.domain.entities.Machine;
 import dev.honokai.data_integrator_backend.domain.entities.Task;
 import dev.honokai.data_integrator_backend.infrastructure.services.SchedulerService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -34,11 +37,12 @@ public class TaskController {
     }
 
     @PostMapping
+    // @todo refatorar, controller está sabendo de mais da implementação
     public ResponseEntity<Task> create(@RequestBody TaskCreateDto taskToCreate) {
         Optional<Machine> machine = machineService.listOne(taskToCreate.getMachine());
-
+        System.out.println(machine.isPresent());
         Task taskCreated = taskService.create(new Task(machine.get(), taskToCreate.getNetworkPath(),
-                taskToCreate.getScanInterval(), taskToCreate.isSingleFile(), taskToCreate.isActive()));
+                taskToCreate.getScanInterval(), taskToCreate.isSingleFile(), taskToCreate.isActive(), taskToCreate.getFileFilter()));
 
         return ResponseEntity.ok(taskCreated);
     }
@@ -58,5 +62,19 @@ public class TaskController {
     public ResponseEntity<TaskResponseDto> edit(@PathVariable String taskId) {
         return taskService.findById(taskId).map(task -> ResponseEntity.ok(new TaskResponseDto(task)))
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{taskId}")
+    public ResponseEntity<TaskResponseDto> update(@PathVariable String taskId, @Valid @RequestBody TaskUpdateDto taskToUpdate) {
+        Task task = taskService.update(taskId, taskToUpdate);
+
+        return ResponseEntity.ok(new TaskResponseDto(task));
+    }
+
+    @GetMapping("/{taskId}/scripts")
+    public ResponseEntity<List<ScriptResponseDto>> list(@PathVariable String taskId) {
+        List<ScriptResponseDto> scripts = taskService.findScriptsRelatedToTask(taskId).stream().map(ScriptResponseDto::new).toList();
+
+        return ResponseEntity.ok(scripts);
     }
 }
--- data-integrator-backend/src/main/java/dev/honokai/data_integrator_backend/domain/entities/Task.java
+++ data-integrator-backend/src/main/java/dev/honokai/data_integrator_backend/domain/entities/Task.java
@@ -8,94 +8,95 @@ import java.util.Set;
 
 @Entity
 public class Task extends Base {
-	@Column(nullable = false)
-	private String networkPath;
+    @Column(nullable = false)
+    private String networkPath;
 
-	@Column(columnDefinition = "bool")
-	private boolean singleFile = false;
+    private int scanInterval = 0;
 
-	private int scanInterval = 0;
+    @ManyToOne
+    @JoinColumn(name = "machine_id", nullable = false)
+    private Machine machine;
 
-	@ManyToOne
-	@JoinColumn(name = "machine_id", nullable = false)
-	private Machine machine;
+    @Embedded
+    private FileFilter fileFilter;
 
-	@Embedded
-	private FileFilter fileFilter;
+    private boolean active;
 
-	private boolean active;
+    private boolean singleFile;
 
-	public Optional<Script> getActiveScript() {
-		return this.scripts.stream().filter(Script::isActive).findFirst();
-	}
+    public Optional<Script> getActiveScript() {
+        return this.scripts.stream().filter(Script::isActive).findFirst();
+    }
 
-	@OneToMany(mappedBy = "task")
-	private Set<Script> scripts = new HashSet<>();
+    @OneToMany(mappedBy = "task")
+    private Set<Script> scripts = new HashSet<>();
 
-	public Task() {
-	}
+    public Task() {
+    }
 
-	public Task(Machine machine, String networkPath, int scanInterval, boolean singleFile, boolean active) {
-		this.machine = machine;
-		this.networkPath = networkPath;
-		this.scanInterval = scanInterval;
-		this.singleFile = singleFile;
-		this.active = active;
-	}
+    public Task(Machine machine, String networkPath, int scanInterval, boolean singleFile, boolean active, FileFilter fileFilter) {
+        this.machine = machine;
+        this.networkPath = networkPath;
+        this.scanInterval = scanInterval;
+        this.singleFile = singleFile;
+        this.active = active;
+        this.fileFilter = fileFilter;
+    }
 
-	public Machine getMachine() {
-		return machine;
-	}
+    public Machine getMachine() {
+        return machine;
+    }
 
-	public void setMachine(Machine machine) {
-		this.machine = machine;
-	}
+    public void setMachine(Machine machine) {
+        this.machine = machine;
+    }
 
-	public String getNetworkPath() {
-		return networkPath;
-	}
+    public String getNetworkPath() {
+        return networkPath;
+    }
 
-	public void setNetworkPath(String networkPath) {
-		this.networkPath = networkPath;
-	}
+    public void setNetworkPath(String networkPath) {
+        this.networkPath = networkPath;
+    }
 
-	public Set<Script> getScripts() {
-		return scripts;
-	}
+    public Set<Script> getScripts() {
+        return scripts;
+    }
 
-	public void setScripts(Set<Script> scripts) {
-		this.scripts = scripts;
-	}
+    public void setScripts(Set<Script> scripts) {
+        this.scripts = scripts;
+    }
 
-	public boolean isSingleFile() {
-		return singleFile;
-	}
 
-	public void setSingleFile(boolean singleFile) {
-		this.singleFile = singleFile;
-	}
+    public int getScanInterval() {
+        return scanInterval;
+    }
 
-	public int getScanInterval() {
-		return scanInterval;
-	}
+    public void setScanInterval(int scanInterval) {
+        this.scanInterval = scanInterval;
+    }
 
-	public void setScanInterval(int scanInterval) {
-		this.scanInterval = scanInterval;
-	}
+    public FileFilter getFileFilter() {
+        return fileFilter;
+    }
 
-	public FileFilter getFileFilter() {
-		return fileFilter;
-	}
+    public void setFileFilter(FileFilter fileFilter) {
+        this.fileFilter = fileFilter;
+    }
 
-	public void setFileFilter(FileFilter fileFilter) {
-		this.fileFilter = fileFilter;
-	}
+    public boolean isActive() {
+        return active;
+    }
 
-	public boolean isActive() {
-		return active;
-	}
+    public void setActive(boolean active) {
+        this.active = active;
+    }
 
-	public void setActive(boolean active) {
-		this.active = active;
-	}
+    public boolean isSingleFile() {
+        return singleFile;
+    }
+
+    public void setSingleFile(boolean singleFile) {
+        this.singleFile = singleFile;
+    }
 }
