09854dc9d5fbb1c1bed51c837a9a5a317a25f014
Honokai <Emerson_Hatred@hotmail.com>
2025-07-16 13:46:28 -0300

    pagina de criacao, implementacao de salvamento da entidade, ainda necessita de validacao dos campos, e tratamento de erro

--- data-integrator/app/scripts/[scriptId]/page.tsx
+++ data-integrator/app/scripts/[scriptId]/page.tsx
@@ -16,9 +16,10 @@ import { Switch } from "@/components/ui/switch";
 import { Textarea } from "@/components/ui/textarea";
 import { Script } from "@/lib/types";
 import apiClient from "@/services/api/apiClient";
+import { updateScript } from "@/services/api/endpoints/script";
 import { ViewUpdate } from "@codemirror/view";
-import { error } from "console";
-import { FileTerminalIcon, Terminal } from "lucide-react";
+import { CopyIcon, FileTerminalIcon, SaveIcon, Terminal } from "lucide-react";
+import Link from "next/link";
 import { useParams } from "next/navigation";
 import React, { useEffect, useMemo, useState } from "react";
 
@@ -75,8 +76,15 @@ export default function ScriptsShowPage() {
     []
   );
 
+  const onFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { data, status } = await updateScript(script);
+    if (data != null) {
+      alert("Script atualizado");
+    }
+  };
+
   useEffect(() => {
-    console.log(scriptId);
     fetchScriptData();
   }, [scriptId]);
 
@@ -94,7 +102,14 @@ export default function ScriptsShowPage() {
     <main className="flex-1 container mx-auto pt-6">
       <Card>
         <CardHeader>
-          <CardTitle className="text-2xl">Script</CardTitle>
+          <CardTitle className="text-2xl flex items-center gap-2">
+            Script
+            <Button variant="ghost" asChild className="p-0 m-0">
+              <Link href={`/scripts/create?copy=${scriptId}`}>
+                <CopyIcon size={20} />
+              </Link>
+            </Button>
+          </CardTitle>
           <CardDescription className="text-muted-foreground flex justify-between">
             <span>Id: {script?.id}</span>
             <span>Version: {script?.version}</span>
@@ -102,71 +117,80 @@ export default function ScriptsShowPage() {
         </CardHeader>
         <CardContent>
           <section title="Script editable details">
-            <form action="#">
-              <div className="grid grid-cols-3 gap-5">
-                <div className="w-fit">
-                  <Label htmlFor="version" className="mb-2">
-                    Version
-                  </Label>
-                  <Input
-                    id="version"
-                    name="version"
-                    readOnly
-                    disabled
-                    value={script?.version ?? "Loading..."}
-                    className="cursor-not-allowed"
-                  />
-                </div>
-                <div className="">
-                  <Label htmlFor="active" className="mb-2">
-                    Active
-                  </Label>
-                  <Switch
-                    id="active"
-                    name="active"
-                    className="cursor-pointer"
-                    checked={script?.active}
-                    onCheckedChange={(checked) => {
-                      setScript({ ...script, active: checked as boolean });
-                    }}
-                  />
-                </div>
-              </div>
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <section title="Script code editor" className="">
-                  <p className="text-xl pt-4">Your code</p>
-                  {memoizedCodeEditor}
-                </section>
-                <section>
-                  <p className="text-xl pt-4">File contents (csv, txt)</p>
-                  <Textarea
-                    className="flex-1 mt-4 font-mono h-96"
-                    placeholder="Want to test your Python code? Enter the sample text to be analyzed here."
-                    value={textAreaValue}
-                    onChange={onChangeTextArea}
-                    rows={9}
-                  />
-                  <div className="flex justify-end mt-2">
-                    {testCodeStatus.state != null && (
-                      <Alert className={`mt-2 text-success`}>
-                        <Terminal />
-                        <AlertTitle>Output</AlertTitle>
-                        <AlertDescription>
-                          <pre>{testCodeStatus.message}</pre>
-                        </AlertDescription>
-                      </Alert>
-                    )}
-                    <Button
-                      variant="ghost"
-                      type="button"
-                      onClick={() => testCode()}
-                    >
-                      <FileTerminalIcon size={20} /> Run
-                    </Button>
+            {script != null ? (
+              <form action="#" onSubmit={onFormSubmit}>
+                <div className="grid grid-cols-3 gap-5">
+                  <div className="w-fit">
+                    <Label htmlFor="version" className="mb-2">
+                      Version
+                    </Label>
+                    <Input
+                      id="version"
+                      name="version"
+                      readOnly
+                      disabled
+                      value={script?.version ?? "Loading..."}
+                      className="cursor-not-allowed"
+                    />
                   </div>
-                </section>
-              </div>
-            </form>
+                  <div className="">
+                    <Label htmlFor="active" className="mb-2">
+                      Active
+                    </Label>
+                    <Switch
+                      id="active"
+                      name="active"
+                      className="cursor-pointer"
+                      checked={script?.active}
+                      onCheckedChange={(checked) => {
+                        setScript({ ...script, active: checked as boolean });
+                      }}
+                    />
+                  </div>
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                  <section title="Script code editor" className="">
+                    <p className="text-xl pt-4">Your code</p>
+                    {memoizedCodeEditor}
+                  </section>
+                  <section>
+                    <p className="text-xl pt-4">File contents (csv, txt)</p>
+                    <Textarea
+                      className="flex-1 mt-4 font-mono h-96"
+                      placeholder="Want to test your Python code? Enter the sample text to be analyzed here."
+                      value={textAreaValue}
+                      onChange={onChangeTextArea}
+                      rows={9}
+                    />
+                    <div className="flex justify-end mt-2">
+                      {testCodeStatus.state != null && (
+                        <Alert className={`mt-2 text-success`}>
+                          <Terminal />
+                          <AlertTitle>Output</AlertTitle>
+                          <AlertDescription>
+                            <pre>{testCodeStatus.message}</pre>
+                          </AlertDescription>
+                        </Alert>
+                      )}
+                      <Button
+                        variant="ghost"
+                        type="button"
+                        onClick={() => testCode()}
+                      >
+                        <FileTerminalIcon size={20} /> Run
+                      </Button>
+                    </div>
+                  </section>
+                </div>
+                <div>
+                  <Button type="submit">
+                    <SaveIcon /> Save
+                  </Button>
+                </div>
+              </form>
+            ) : (
+              <p>Loading script data</p>
+            )}
           </section>
         </CardContent>
       </Card>
