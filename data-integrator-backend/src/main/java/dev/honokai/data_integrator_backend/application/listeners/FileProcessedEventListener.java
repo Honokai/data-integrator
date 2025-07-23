af08a3b231a8c43dbe9579d546a3bdd2c07cb016
Honokai <Emerson_Hatred@hotmail.com>
2025-07-23 13:54:11 -0300

    criaca de consumidor de evento e adaptacao em Task para relacionar-se com Integrator

--- data-integrator-backend/src/main/java/dev/honokai/data_integrator_backend/application/listeners/FileProcessedEventListener.java
+++ data-integrator-backend/src/main/java/dev/honokai/data_integrator_backend/application/listeners/FileProcessedEventListener.java
@@ -1,7 +1,7 @@
 package dev.honokai.data_integrator_backend.application.listeners;
 
-import dev.honokai.data_integrator_backend.domain.entities.RestApiIntegrator;
 import dev.honokai.data_integrator_backend.domain.events.FileProcessedEvent;
+import dev.honokai.data_integrator_backend.infrastructure.integrators.RestApiIntegrator;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
