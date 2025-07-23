e5182918de4dd9d64667a1bbeb1b7e865b7b7733
Honokai <Emerson_Hatred@hotmail.com>
2025-07-23 13:35:39 -0300

    novo enum para definir qual o tipo de estratégia utilizar quando escanendo itens, e metodo de leitura para recuperacao dos valores existentes pelo frontend

--- data-integrator-backend/src/main/java/dev/honokai/data_integrator_backend/application/controllers/EnumController.java
+++ data-integrator-backend/src/main/java/dev/honokai/data_integrator_backend/application/controllers/EnumController.java
@@ -1,6 +1,7 @@
 package dev.honokai.data_integrator_backend.application.controllers;
 
 import dev.honokai.data_integrator_backend.domain.enums.FileFilterType;
+import dev.honokai.data_integrator_backend.domain.enums.SourceType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +13,16 @@ import java.util.stream.Stream;
 @RestController
 @RequestMapping("/api/enums")
 public class EnumController {
-    @GetMapping("/fileFilterTypes")
-    public ResponseEntity<List<String>> fileFilterTypesIndex() {
+    @GetMapping("/file-filter-types")
+    public ResponseEntity<List<String>> fileFilterTypes() {
         List<String> availableValues = Stream.of(FileFilterType.values()).map(Enum::name).toList();
+
+        return ResponseEntity.ok(availableValues);
+    }
+
+    @GetMapping("/file-source-types")
+    public ResponseEntity<List<String>> fileSourceTypes() {
+        List<String> availableValues = Stream.of(SourceType.values()).map(Enum::name).toList();
 
         return ResponseEntity.ok(availableValues);
     }
